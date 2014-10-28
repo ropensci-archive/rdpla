f94f50ed2261080cef6911dfb26ad877195eb3e4
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-28 12:50:55 -0700

    pkg name changes, moved files around

--- R/rdpla.R
+++ R/rdpla.R
@@ -1,14 +1,14 @@
 #' R Client for the Digital Public Library of America (DPLA).
 #'
-#' @name dpla-package
-#' @aliases dpla-package
-#' @aliases dpla
+#' @name rdpla-package
+#' @aliases rdpla-package
+#' @aliases rdpla
 #' @description Interact with the Digital Public Library of America (DPLA) REST API from R,
 #' including search, etc, etc.
 #' @docType package
 #' @details
 #' \tabular{ll}{
-#'  Package: \tab dpla\cr
+#'  Package: \tab rdpla\cr
 #'  Type: \tab Package\cr
 #'  Version: \tab 0.0.1.99\cr
 #'  Date: \tab 2014-10-28\cr
