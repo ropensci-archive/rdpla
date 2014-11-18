86effc34d5b69b0a0a2d883410cb4246e1d07360
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-17 16:00:33 -0800

    changes to dpla_items no longer does paging automatically, users have to do that themselves changed limit param to page_size, default to 100, max is now 500 changed by dpla added a bunch of params, more still to add fixed some parsing issues

--- R/dpla_items.r
+++ R/dpla_items.r
@@ -6,17 +6,40 @@
 #' @export
 #'
 #' @param q Query terms.
-#' @param limit Number of items to return, defaults to 10. Max of 100.
+#' @param description Object description.
+#' @param title Object title.
+#' @param subject Subject.
+#' @param creator Creator
+#' @param type Type of object
+#' @param publisher PUblisher
+#' @param format Format
+#' @param rights Rights
+#' @param contributor Contributor
+#' @param provider Provider
+#' @param sp Query all spatial fields.
+#' @param sp_coordinates Query only coordinates. Of the form <latitude,longitude>
+#' @param sp_city Query by city name.
+#' @param sp_county Query by county name.
+#' @param sp_distance Distance from point defined in the \code{sp_coordinates} field
+#' @param sp_country Query by location country
+#' @param sp_code Query by ISO 3166-2 country code. Codes are included in this package, see
+#' \code{country_codes}. Find out more at \url{https://www.iso.org/obp/ui/#search}.
+#' @param sp_name Location name.
+#' @param sp_region Name of a region, e.g., "Upstate New York" (literal)
+#' @param sp_state ISO 3166-2 code for a U.S. state or territory
+#' @param language One of a name of a language, or an ISO-639 code. Codes are included in this
+#' package, see \code{language_codes}.  Find out more at
+#' \url{http://www-01.sil.org/iso639-3/default.asp}.
+#' @param page_size Number of items to return, defaults to 100. Max of 500.
 #' @param page Page number to return, defaults to NULL.
 #' @param sort_by The default sort order is ascending. Most, but not all fields
 #'    can be sorted on. Attempts to sort on an un-sortable field will return
 #'    the standard error structure with a HTTP 400 status code.
 #' @param fields A vector of the fields to return in the output. The default
 #'    is all fields. See details for options.
-#' @param date.before Date before
-#' @param date.after Date after
-#' @param verbose If TRUE, fun little messages print to console to inform you
-#'    of things.
+#' @param date_before Date before
+#' @param date_after Date after
+#' @param facets Fields to facet on.
 #' @param key Your DPLA API key. Either pass in here, or store in your \code{.Rprofile} file
 #'    and it will be read in on function execution.
 #' @param what One of list or table (dat.frame). (Default: table)
@@ -39,84 +62,115 @@
 #'  \item provider The provider of the object
 #' }
 #' @return A list of length two: meta with the metadata for the call (found, offset [aka start],
-#' and limit [number results returned]), and the resulting data.frame of results.
+#' and page_size [number results returned]), and the resulting data.frame of results.
 #'
 #' @examples \donttest{
 #' # Basic search, "fruit" in any fields
 #' dpla_items(q="fruit")
 #'
 #' # Limit records returned
-#' dpla_items(q="fruit", limit=2)
-#'
-#' # Some verbosity
-#' dpla_items(q="fruit", verbose=TRUE, limit=2)
+#' dpla_items(q="fruit", page_size=2)
 #'
 #' # Return certain fields
 #' dpla_items(q="fruit", verbose=TRUE, fields=c("id","publisher","format"))
 #' dpla_items(q="fruit", fields="subject")
 #'
-#' # Max is 100 per call, but the function handles larger numbers by looping
-#' dpla_items(q="fruit", fields="id", limit=200)
-#' dpla_items(q="fruit", fields=c("id","provider"), limit=200)
-#' out <- dpla_items(q="science", fields=c("id","subject"), limit=400)
-#' head(out$data)
+#' # Max is 500 per call, but you can use combo of page_size and page params
+#' dpla_items(q="fruit", fields="id", page_size=500)$meta$returned
+#' lapply(1:2, function(x) dpla_items(q="fruit", fields="id", page_size=500, page=x)$meta$returned)
+#' out <- lapply(1:2, function(x) dpla_items(q="fruit", fields="id", page_size=500, page=x))
+#' lapply(out, function(y) head(y$data))
 #'
 #' # Search by date
-#' out <- dpla_items(q="science", date.before=1900, limit=200)
+#' out <- dpla_items(q="science", date_before=1900, page_size=200)
 #' head(out$data)
 #'
-#' # Spatial search
-#' dpla_items(q='Boston', fields='spatial')
+#' # Search by various fields
+#' dpla_items(description="obituaries", page_size=2, fields="description")
+#' dpla_items(title="obituaries", page_size=2, fields="title")
+#' dpla_items(subject="yodeling", page_size=2, fields="subject")
+#' dpla_items(creator="Holst-Van der Schalk", page_size=2, fields="creator")
+#' dpla_items(type="text", page_size=2, fields="type")
+#' dpla_items(publisher="Leningrad", page_size=2, fields="publisher")
+#' dpla_items(rights="unrestricted", page_size=2, fields="rights")
+#' dpla_items(provider="HathiTrust", page_size=2, fields="provider")
 #'
-#' # things
-#' dpla_items(q="*:*", limit=5, what="list")
+#' ## don't seem to work
+#' dpla_items(contributor="Smithsonian", page_size=2, fields="contributor")
+#' dpla_items(format="Electronic resource", page_size=2, fields="format")
+#'
+#' # Spatial search
+#' ## sp searches all spatial fields, or search on specific fields, see those params with sp_*
+#' dpla_items(sp='Boston', page_size=2)
+#' dpla_items(sp_state='Hawaii', page_size=2)
+#' dpla_items(sp_state='Massachusetts OR Hawaii', page_size=2)
+#' dpla_items(sp_coordinates='40,-100', page_size=2)
+#' dpla_items(sp_country='Canada', page_size=2)
+#' dpla_items(sp_county='Sacramento', page_size=2)
+#'
+#' # language search
+#' dpla_items(language='Russian')$meta
+#' dpla_items(language='rus')$meta
+#' dpla_items(language='English')$meta
+#'
+#' # Faceting
+#' dpla_items(facets="rights", page_size=5, what="list")
 #' }
 
-dpla_items <- function(q=NULL, verbose=FALSE, fields=NULL, limit=10, page=NULL,
-  sort_by=NULL, date.before=NULL, date.after=NULL, key=getOption("dplakey"),
-  what="table", ...)
+dpla_items <- function(q=NULL, description=NULL, title=NULL, subject=NULL, creator=NULL,
+  type=NULL, publisher=NULL, format=NULL, rights=NULL, contributor=NULL, provider=NULL, sp=NULL,
+  sp_coordinates=NULL, sp_city=NULL, sp_county=NULL,
+  sp_distance=NULL, sp_country=NULL, sp_code=NULL, sp_name=NULL, sp_region=NULL, sp_state=NULL,
+  fields=NULL, sort_by=NULL, date_before=NULL, date_after=NULL, language=NULL,
+  facets=NULL, page_size=100, page=NULL, key=getOption("dplakey"), what="table", ...)
 {
   fields2 <- fields
 
   if(!is.null(fields)){
     fieldsfunc <- function(x){
       if(x %in% c("title","description","subject","creator","type","publisher",
-                  "format","rights","contributor","spatial")) {
+                  "format","rights","contributor","spatial","spatial.coordinates","spatial.city",
+                  "spatial.county","spatial.distance","spatial.country","spatial.iso3166-2",
+                  "spatial.name","spatial.region","spatial.state","language")) {
         paste("sourceResource.", x, sep="") } else { x }
     }
     fields <- paste(sapply(fields, fieldsfunc, USE.NAMES=FALSE), collapse=",")
-  } else {NULL}
-
-  if(!limit > 100){
-    args <- dcomp(list(api_key=key, q=q, page_size=limit, page=page, fields=fields,
-                         sourceResource.date.before=date.before,
-                         sourceResource.date.after=date.after))
-    temp <- dpla_GET(paste0(dpbase(), "items"), args, ...)
-    hi <- data.frame(temp[c('count','limit')], stringsAsFactors = FALSE)
-    names(hi) <- c('found','returned')
-    if(verbose)
-      message(paste(hi$count, " objects found, started at ", hi$start, ", and returned ", hi$limit, sep=""))
-    dat <- temp[[4]] # collect data
-  } else
-  {
-    maxpage <- ceiling(limit/100)
-    page_vector <- seq(1,maxpage,1)
-    argslist <- lapply(page_vector, function(x) dcomp(list(api_key=key, q=q, page_size=100, page=x, fields=fields, sourceResource.date.before=date.before, sourceResource.date.after=date.after)))
-    out <- lapply(argslist, function(x) dpla_GET(paste0(dpbase(), "items"), args = x, ...))
-    hi <- data.frame(found=out[[1]]$count, stringsAsFactors = FALSE)
-    hi$returned <- sum(sapply(out, function(x) length(x$docs)))
-    if(verbose)
-      message(paste(hi$count, " objects found, started at ", hi$start, ", and returned ", sum(hi[,c(5,6)]), sep=""))
-    dat <- do.call(c, lapply(out, function(x) x[[4]])) # collect data
   }
+
+  args <- dcomp(list(api_key=key, q=q, page_size=page_size, page=page, fields=fields,
+                     provider=provider,
+                     sourceResource.description=description,
+                     sourceResource.title=title,
+                     sourceResource.subject=subject,
+                     sourceResource.creator=creator,
+                     sourceResource.type=type,
+                     sourceResource.publisher=publisher,
+                     sourceResource.format=format,
+                     sourceResource.rights=rights,
+                     sourceResource.contributor=contributor,
+                     sourceResource.spatial=sp,
+                     sourceResource.spatial.coordinates=sp_coordinates,
+                     sourceResource.spatial.city=sp_city,
+                     sourceResource.spatial.county=sp_county,
+                     sourceResource.spatial.distance=sp_distance,
+                     sourceResource.spatial.country=sp_country,
+                     `sourceResource.spatial.iso3166-2`=sp_code,
+                     sourceResource.spatial.name=sp_name,
+                     sourceResource.spatial.region=sp_region,
+                     sourceResource.spatial.state=sp_state,
+                     sourceResource.language=language,
+                     sourceResource.date.before=date_before,
+                     sourceResource.date.after=date_after,
+                     facets=facets))
+  temp <- dpla_GET(url=paste0(dpbase(), "items"), args, ...)
+  hi <- setNames(data.frame(temp[c('count','start','limit')], stringsAsFactors = FALSE), c('found','start','returned'))
+  dat <- temp$docs
 
   if(what == "list"){
     structure(list(meta=hi, data=dat))
   } else {
     output <- do.call(rbind.fill, lapply(dat, getdata, flds=fields))
-
-    if(is.null(fields)){ list(meta=hi, data=output) } else
-    {
+    if(is.null(fields)){ list(meta=hi, data=output) } else {
       output2 <- output[,names(output) %in% fields2]
       # convert one column factor string to data.frame (happens when only one field is requested)
       if(class(output2) %in% "factor"){
@@ -131,20 +185,23 @@ dpla_items <- function(q=NULL, verbose=FALSE, fields=NULL, limit=10, page=NULL,
 # function to process data for each element
 getdata <- function(y, flds){
   process_res <- function(x){
-    reduce1 <- function(x) if(length(x) > 1) paste(as.character(x), collapse=";") else x
-    id <- x$id
+    reduce1 <- function(x){
+      x <- unlist(x)
+      if(length(x) > 1) paste(as.character(x), collapse=";") else x
+    }
+    id <- reduce1(x$identifier)
     title <- reduce1(x$title)
-    description <- x$description
+    description <- reduce1(x$description)
     subject <- if(length(x$subject)>1){paste(as.character(unlist(x$subject)), collapse=";")} else {x$subject[[1]][["name"]]}
     language <- x$language[[1]][["name"]]
-    format <- x$format
+    format <- reduce1(x$format)
     collection <- if(any(names(x$collection) %in% "name")) {x$collection[["name"]]} else {"no collection name"}
-    type <- x$type
+    type <- reduce1(x$type)
     date <- x$date[[1]]
-    publisher <- x$publisher
+    publisher <- reduce1(x$publisher)
     provider <- x$provider[["name"]]
     creator <- reduce1(x$creator)
-    rights <- x$rights
+    rights <- reduce1(x$rights)
 
     replacenull <- function(y) if(is.null(y) || length(y) == 0) "no content" else y
     ents <- list(id,title,description,subject,language,format,collection,type,provider,publisher,creator,rights,date)
@@ -164,8 +221,7 @@ getdata <- function(y, flds){
 
   if(is.null(flds)){
     id <- y$id
-    provider <- data.frame(t(y$provider), stringsAsFactors = FALSE)
-    names(provider) <- c("provider_url","provider_name")
+    provider <- setNames(data.frame(t(y$provider), stringsAsFactors = FALSE), c("provider_url","provider_name"))
     score <- y$score
     url <- y$isShownAt
     sourceResource <- y$sourceResource
