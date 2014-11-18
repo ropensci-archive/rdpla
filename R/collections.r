bd5dbc662925d22080d42ea323a35541d14a8598
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-18 07:37:29 -0800

    added title and description params to collections fxn, #11

--- R/collections.r
+++ R/collections.r
@@ -3,12 +3,14 @@
 #' @export
 #'
 #' @param q Query terms.
+#' @param title Query in the title field
+#' @param description Query in the description field
 #' @param fields A vector of the fields to return in the output. The default
 #' @param sort_by The default sort order is ascending. Most, but not all fields
 #'    can be sorted on. Attempts to sort on an un-sortable field will return
 #'    the standard error structure with a HTTP 400 status code.
 #'    is all fields. See details for options.
-#' @param limit Number of items to return, defaults to 10. Max of 100.
+#' @param page_size Number of items to return, defaults to 10. Max of 500.
 #' @param page Page number to return, defaults to NULL.
 #' @param key Your DPLA API key. Either pass in here, or store in your \code{.Rprofile} file
 #'    and it will be read in on function execution.
@@ -19,12 +21,14 @@
 #' collections(q="university of texas", page_size=2)
 #' collections(q="university of texas", fields='id', page_size=2)
 #' collections(q="university of texas", sort_by='title', page_size=5)
+#' collections(title="paso")
+#' collections(description="east")
 #' }
 
-collections <- function(q=NULL, fields=NULL, sort_by=NULL, page_size=10, page=NULL,
-  key=getOption("dplakey"), ...)
+collections <- function(q=NULL, title=NULL, description=NULL, fields=NULL, sort_by=NULL,
+  page_size=10, page=NULL, key=getOption("dplakey"), ...)
 {
-  args <- dcomp(list(api_key=key, q=q, page_size=page_size, page=page, fields=fields, sort_by=sort_by))
+  args <- dcomp(list(api_key=key, q=q, title=title, description=description, page_size=page_size, page=page, fields=fields, sort_by=sort_by))
   res <- dpla_GET(paste0(dpbase(), "collections"), args, ...)
   meta <- data.frame(found=res$count, returned=res$limit, stringsAsFactors = FALSE)
   dat <- do.call(rbind.fill, lapply(res$docs, parse_coll))
